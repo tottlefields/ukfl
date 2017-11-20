af4c668c1bf9ee5275b7f9972c5841a20d7245d4
Ellen Schofield <tottlefields@gmail.com>
2017-11-20 17:03:31 +0000

    set joined date as date actually joined rather than the date direct debit starts on

--- ukfl-theme/gocardless-pro.php
+++ ukfl-theme/gocardless-pro.php
@@ -2,7 +2,7 @@
 
 function mandate_validator($input){
         $current_user = wp_get_current_user();
-        update_user_meta( $current_user->ID, 'ukfl_cgp_customer_ref', $input->links->mandate);
+        update_user_meta( $current_user->ID, 'ukfl_cgp_customer_ref', $input->links->customer);
 }
 
 function subscription_validator($input){
@@ -10,7 +10,7 @@ function subscription_validator($input){
         $current_user = wp_get_current_user();
         if (preg_match('/Membership/', $input->name)){
 			$current_user->add_role( 'ukfl_member' );
-			add_user_meta( $current_user->ID, 'ukfl_date_joined', $input->start_date, 1 );
+			add_user_meta( $current_user->ID, 'ukfl_date_joined', date('Y-m-d'), 1 );
 			add_user_meta( $current_user->ID, 'ukfl_date_renewal', $input->upcoming_payments[1]->charge_date, 1 );
 			add_user_meta( $current_user->ID, 'ukfl_mandate_membership', $input->links->mandate, 1 );
         }
