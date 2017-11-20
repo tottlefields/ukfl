19785f66bf4c681197b7c9604c559c25f34a6d40
Ellen SChofield <online@ukflyball.org.uk>
2017-11-20 16:37:17 +0000

    fixed bug with ukfl number

--- ukfl-theme/theme-my-login/register-form.php
+++ ukfl-theme/theme-my-login/register-form.php
@@ -9,10 +9,8 @@ global $wpdb;
 $year = date('y');
 $sql = "select max(user_login) from wplt_users where user_login like '$year%'";
 $ukfl_no = $wpdb->get_var( $sql );
-if (!isset($ukfl_no)){ $ukfl_no = str_pad($year, 6. '0', STR_PAD_RIGHT); }
+if (!isset($ukfl_no)){ $ukfl_no = str_pad($year, 6, '0', STR_PAD_RIGHT); }
 $ukfl_no++;
-
-print_r($_POST);
 ?>
 <div class="alert alert-warning">Your registration email should be sent within minutes of you completing this form. Please check your spam/junk folders. If you haven't received anything within 30 minutes, please <a href="/contact/">contact us</a>.</div>
 <div class="tml tml-register" id="theme-my-login<?php $template->the_instance(); ?>">
