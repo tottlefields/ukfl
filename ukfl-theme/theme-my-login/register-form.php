23ae63ec19f6fca180f3322093c5cd4bc93496e9
Ellen Schofield <tottlefields@gmail.com>
2017-11-20 21:47:23 +0000

    created generate_ukfl_number function for use across the site

--- ukfl-theme/theme-my-login/register-form.php
+++ ukfl-theme/theme-my-login/register-form.php
@@ -5,12 +5,7 @@ Theme My Login will always look in your theme's directory first, before using th
 */
 ?>
 <?php
-global $wpdb;
-$year = date('y');
-$sql = "select max(user_login) from wplt_users where user_login like '$year%'";
-$ukfl_no = $wpdb->get_var( $sql );
-if (!isset($ukfl_no)){ $ukfl_no = str_pad($year, 6, '0', STR_PAD_RIGHT); }
-$ukfl_no++;
+$ukfl_no = generate_ukfl_number();
 ?>
 <div class="alert alert-warning">Your registration email should be sent within minutes of you completing this form. Please check your spam/junk folders. If you haven't received anything within 30 minutes, please <a href="/contact/">contact us</a>.</div>
 <div class="tml tml-register" id="theme-my-login<?php $template->the_instance(); ?>">
@@ -31,16 +26,26 @@ $ukfl_no++;
 		<div class="form-group">
 			<label class="col-sm-2 control-label" for="first_name<?php $template->the_instance(); ?>"><?php _e( 'First name', 'theme-my-login' ) ?></label>
 			<div class="col-sm-3">
-				<input type="text" name="first_name" id="first_name<?php $template->the_instance(); ?>" class="input form-control" value="<?php $template->the_posted_value( 'first_name' ); ?>" size="20" tabindex="20" />
+				<input type="text" name="first_name" id="first_name<?php $template->the_instance(); ?>" class="input form-control" value="<?php $template->the_posted_value( 'first_name' ); ?>" size="20" />
 			</div>
                         <label class="col-sm-2 control-label" for="last_name<?php $template->the_instance(); ?>"><?php _e( 'Last name', 'theme-my-login' ) ?></label>
 			<div class="col-sm-5">
-				<input type="text" name="last_name" id="last_name<?php $template->the_instance(); ?>" class="input form-control" value="<?php $template->the_posted_value( 'last_name' ); ?>" size="20" tabindex="20" />
+				<input type="text" name="last_name" id="last_name<?php $template->the_instance(); ?>" class="input form-control" value="<?php $template->the_posted_value( 'last_name' ); ?>" size="20" />
 			</div>
                 </div>
+		
+		<div class="form-group">
+			<label class="col-sm-2 control-label" for="pass1<?php $template->the_instance(); ?>"><?php _e( 'Password', 'theme-my-login' ); ?></label>
+			<div class="col-sm-3">
+	                        <input autocomplete="off" name="pass1" id="pass1<?php $template->the_instance(); ?>" class="input" size="20" value="" type="password" />
+			</div>
+                        <label class="col-sm-2 control-label" for="pass2<?php $template->the_instance(); ?>"><?php _e( 'Confirm Password', 'theme-my-login' ); ?></label>
+			<div class="col-sm-5">
+	                        <input autocomplete="off" name="pass2" id="pass2<?php $template->the_instance(); ?>" class="input" size="20" value="" type="password" />
+			</div>
+		</div>
 
-
-		<?php do_action( 'register_form' ); ?>
+		<?php #do_action( 'register_form' ); ?>
 
 		<p class="tml-registration-confirmation" id="reg_passmail<?php $template->the_instance(); ?>"><?php echo apply_filters( 'tml_register_passmail_template_message', __( 'Registration confirmation will be e-mailed to you.', 'theme-my-login' ) ); ?></p>
 
