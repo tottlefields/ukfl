1c8115ef3b6f8f84a046911ae4961c832c33c728
Ellen Schofield <tottlefields@gmail.com>
2017-11-27 15:26:44 +0000

    membership updates

--- ukfl-theme/page-templates/membership_individual.php
+++ ukfl-theme/page-templates/membership_individual.php
@@ -14,8 +14,8 @@ get_template_part('index', 'bannerstrip');
 				<div class="page-content">
 						<article id="page-<?php the_ID(); ?>" <?php post_class('page'); ?> > 			
 							<div class="entry-content">
-								<p>Many thanks for joining UKFL and selecting an Individual Membership. Your direct debit mandate and payment plan have been successfully setup (see reference below). Your new UKFL membership number and password should have been e-mailed to you, using the email address you have provided. Thank you.</p>
-								<?php the_post(); the_content(); ?>
+								<p>Many thanks for joining UKFL and selecting an Individual Membership. Your direct debit mandate and payment plan have been successfully setup (see reference below). Your new UKFL membership number and password should have been e-mailed to you, using the email address you have provided. If you haven't received this within the next 30 mins please <a href="/contact/">contact us</a>.</p>
+							<div class="alert alert-success" role="alert"><?php the_post(); the_content(); ?></div>
 								<p>Please <a href="/login/">click here</a> to login to your new account.</p>
 							</div>
 					</article>
--- ukfl-theme/page-templates/membership_joint.php
+++ ukfl-theme/page-templates/membership_joint.php
@@ -1,36 +1,12 @@
 <?php
 //Template Name: Membership (Joint)
 
-$current_user = wp_get_current_user();
 if (isset($_REQUEST['add_joint_member'])){
 	if ( email_exists( $_REQUEST['user_email'] ) ) {
 		$error = 'A username with that email address ('.$_REQUEST['user_email'].') already exists on the system. If you want this account to be linked to your joint account, please contact UKFL to have them set this up for you.';
 	}else{
-		$password = wp_generate_password( 12, true );
-		$user_id = wp_create_user ( $_REQUEST['user_login'], $password, $_REQUEST['user_email'] );
-		wp_update_user(
-			array(
-				'ID'		=> $user_id,
-				'first_name'	=> $_REQUEST['first_name'],
-				'last_name'	=> $_REQUEST['last_name'],
-				'nickname'	=> $_REQUEST['first_name'].' '.$_REQUEST['last_name']
-			)
-		);
-		$user = new WP_User($user_id);
-		$user->set_role('subscriber');
-		$user->add_role('ukfl_member');
-		$msg = 'Dear '.$_REQUEST['first_name'].',
-You have had an account set up for you on the <a href="'.home_url().'">UKFL website</a> as part of '.$current_user->display_name.'\'s joint membership. You do not need to do anthing further to be able to enjoy UKFL.
-
-Please feel free to <a href="'.esc_url( wp_login_url() ).'">login to your account</a> and start adding your dogs.
-
-Your password is: <strong>'.$password.'</strong>
-Your UKFL Number is: <strong>'.$_REQUEST['user_login'].'</strong>
-
-We look forward to racing with you in UKFL.';
-		$headers = array('Content-Type: text/html; charset=UTF-8');
-		wp_mail( $_REQUEST['user_email'], '[UK Flyball League] Welcome to UKFL!', $msg, $headers );
-		wp_safe_redirect('/account/');
+		create_ukfl_member($_REQUEST['first_name'], $_REQUEST['last_name'], $_REQUEST['user_email']);
+		wp_safe_redirect('/login/');
 		exit;
 	}
 }
@@ -45,14 +21,12 @@ get_template_part('index', 'bannerstrip');
 		<div class="row">
 			<!--Blog Posts-->
 			<div class="col-md-12 col-xs-12">
-				<div class="page-content">
-					<p>Many thanks for joining UKFL and selecting a Joint Membership. using the email address you have provided. THank you.</p>
-					
+				<div class="page-content">					
 						<article id="post-<?php the_ID(); ?>" <?php post_class('post'); ?> > 					
 						<div class="entry-content">
-							<p>Many thanks for joining UKFL and selecting a Joint Membership. Your direct debit mandate and payment plan have been successfully setup (see reference below). Your new UKFL membership number and password should have been e-mailed to you, using the email address you have provided. Thank you.</p>
-							<?php the_post(); the_content(); ?>
-							<p>Please fill in the details below for the other party on your joint membership to set them up their own account on the UKFL website. Their password and UKFL membership number will be emailed to them separately</p>
+							<p>Many thanks for joining UKFL and selecting a Joint Membership. Your direct debit mandate and payment plan have been successfully setup (see reference below). Your new UKFL membership number and password should have been e-mailed to you, using the email address you have provided. If you haven't received this within the next 30 mins please <a href="/contact/">contact us</a>.</p>
+							<div class="alert alert-success" role="alert"><?php the_post(); the_content(); ?></div>
+							<p>Please fill in the details below for the other party on your joint membership to set them up their own account on the UKFL website. Their password and UKFL membership number will be emailed to them separately.</p>
 							<div class="tml tml-register" id="theme-my-login">
 								<form class="form form-horizontal" name="registerform" id="registerform" method="post">
 									<div class="form-group">
@@ -60,17 +34,14 @@ get_template_part('index', 'bannerstrip');
 										<div class="col-sm-5">
 											<input type="text" name="user_email" id="user_email" class="input form-control" value="">
 										</div>
-									</div>
-									<div class="form-group">
-										<label class="col-sm-2 control-label" for="first_name">First name</label>
+										<label class="col-sm-2 control-label" for="first_name">Name</label>
 										<div class="col-sm-3">
-											<input type="text" name="first_name" id="first_name" class="input form-control" value="" >
+											<input type="text" placeholder="First Name" name="first_name" id="first_name" class="input form-control" value="" >
 										</div>
-										<label class="col-sm-2 control-label" for="last_name">Last name</label>
 										<div class="col-sm-5">
-											<input type="text" name="last_name" id="last_name" class="input form-control" value="">
+											<input type="text" placeholder="Last Name" name="last_name" id="last_name" class="input form-control" value="">
 										</div>
-								        </div>
+								    </div>
 									<p class="tml-submit-wrap">
 										<input type="submit" class="btn btn-success btn-busiprof pull-right" name="add_joint_member" id="add_joint_member" value="Continue">
 									</p>
