0f84154233cee038ba1ef1608b2467b6079b286e
Ellen Schofield <tottlefields@gmail.com>
2017-11-27 13:00:03 +0000

    adding cpg functiosn for ukfl

--- ukfl-theme/gocardless-pro.php
+++ ukfl-theme/gocardless-pro.php
@@ -18,4 +18,21 @@ function subscription_validator($input){
 add_action('gcp_successful_mandate_setup', 'mandate_validator');
 add_action('gcp_successful_payment_plan', 'subscription_validator');
 
+
+require_once '/home/ec2-user/vhosts/ukflyball.org.uk/www/wp-content/plugins/gocardless-pro/lib/gcp/vendor/autoload.php';
+
+function _create_sandbox_client(){
+	$access_token = 'sandbox_2uv9or7FYuFlMG97I_M04c4KgDRggrtrUF6cps8S';
+	$client = new \GoCardlessPro\Client(array(
+			'access_token' => $access_token,
+			'environment'  => \GoCardlessPro\Environment::SANDBOX
+	));
+	return $client;
+}
+
+function cpg_ukfl_get_customer($cust_ref){
+	$client = _create_sandbox_client();
+	$customer = $client->customers()->get($cust_ref);
+}
+
 ?>
