e9e2e545e2f27d8855b68ea0de49e18d512ebc24
Ellen Schofield <tottlefields@gmail.com>
2017-11-28 22:14:27 +0000

    changed club to team

--- ukfl-plugin/posttypes.php
+++ ukfl-plugin/posttypes.php
@@ -4,14 +4,14 @@
 // Our custom post type function
 function register_custom_posttypes() {
 
-	$club_args = array(
+	$team_args = array(
 			'labels' => array(
-					'name' 				=> __( 'Clubs', 'ukfl' ),
-					'singular_name' 	=> __( 'Club', 'ukfl' ),
-					'add_new_item' 		=> __( 'Add New Club', 'ukfl' ),
-					'edit_item' 		=> __( 'Edit Club', 'ukfl' ),
+					'name' 				=> __( 'Teams', 'ukfl' ),
+					'singular_name' 	=> __( 'Team', 'ukfl' ),
+					'add_new_item' 		=> __( 'Add New Team', 'ukfl' ),
+					'edit_item' 		=> __( 'Edit Team', 'ukfl' ),
 					'new_item' 			=> __( 'New', 'ukfl' ),
-					'view_item' 		=> __( 'View Club', 'ukfl' ),
+					'view_item' 		=> __( 'View Team', 'ukfl' ),
 					'search_items' 		=> __( 'Search', 'ukfl' ),
 					'not_found' 		=> __( 'No results found.', 'ukfl' ),
 					'not_found_in_trash' 	=> __( 'No results found.', 'ukfl' ),
@@ -22,12 +22,12 @@ function register_custom_posttypes() {
 			),
 			'public' 		=> true,
 			'show_ui' 		=> true,
-			//'capability_type' 	=> 'ukfl_club',
+			//'capability_type' 	=> 'ukfl_team',
 			//'map_meta_cap' 	=> true,
 			'publicly_queryable' 	=> true,
 			'exclude_from_search' 	=> false,
 			'hierarchical'		=> false,
-			'rewrite'		=> array( 'slug' => get_option('ukfl_club_slug','club') ),
+			'rewrite'		=> array( 'slug' => get_option('ukfl_team_slug','team') ),
 			'supports' 		=> array( 'title', 'author', 'thumbnail' ),
 			'has_archive' 		=> false,
 			'show_in_nav_menus' 	=> true,
@@ -37,14 +37,14 @@ function register_custom_posttypes() {
 			//'rest_base' 			=> 'teams',
 	);
 
-	$team_args = array(
+	$sub_team_args = array(
 		'labels' => array(
-                        'name'          		=> __( 'Teams', 'ukfl' ),
-                        'singular_name'         => __( 'Team', 'ukfl' ),
-                        'add_new_item'          => __( 'Add New Team', 'ukfl' ),
-                        'edit_item'             => __( 'Edit Team', 'ukfl' ),
+                        'name'          		=> __( 'Sub Teams', 'ukfl' ),
+                        'singular_name'         => __( 'Sub Team', 'ukfl' ),
+                        'add_new_item'          => __( 'Add New Sub Team', 'ukfl' ),
+                        'edit_item'             => __( 'Edit Sub Team', 'ukfl' ),
                         'new_item'              => __( 'New', 'ukfl' ),
-                        'view_item'             => __( 'View Team', 'ukfl' ),
+                        'view_item'             => __( 'View Sub Team', 'ukfl' ),
                         'search_items'          => __( 'Search', 'ukfl' ),
                         'not_found'             => __( 'No results found.', 'ukfl' ),
                         'not_found_in_trash'    => __( 'No results found.', 'ukfl' ),
@@ -54,7 +54,7 @@ function register_custom_posttypes() {
                 'publicly_queryable'    => true,
                 'exclude_from_search'   => false,
                 'hierarchical'          => false,
-                'rewrite'               => array( 'slug' => get_option('ukfl_team_slug','team') ),
+                'rewrite'               => array( 'slug' => get_option('ukfl_sub_team_slug','sub-team') ),
                 'supports'              => array( 'title', 'author' ),
                 'has_archive'           => false,
                 'menu_icon'             => 'dashicons-shield-alt',
@@ -82,8 +82,8 @@ function register_custom_posttypes() {
                 'menu_icon'             => 'dashicons-location-alt',
 	);
 
-	register_post_type('ukfl_club',  $club_args);
 	register_post_type('ukfl_team',  $team_args);
+	register_post_type('ukfl_sub-team',  $sub_team_args);
 	register_post_type('ukfl_event', $event_args);
 }
 // Hooking up our function to theme setup
@@ -91,15 +91,15 @@ add_action( 'init', 'register_custom_posttypes' );
 
 
 
-function is_ukfl_club(){
+function is_ukfl_team(){
 	global $wp_query;
-	if ($wp_query->query_vars['post_type'] == 'ukfl_club') return true;
+	if ($wp_query->query_vars['post_type'] == 'ukfl_team') return true;
 	return false;
 }
 
-function is_ukfl_team(){
+function is_ukfl_sub_team(){
         global $wp_query;
-        if ($wp_query->query_vars['post_type'] == 'ukfl_team') return true;
+        if ($wp_query->query_vars['post_type'] == 'ukfl_sub-team') return true;
         return false;
 }
 
@@ -108,7 +108,7 @@ function is_ukfl_team(){
 add_filter('posts_orderby', 'club_team_orderby');
 function club_team_orderby($sql){
 	global $wpdb, $wp_query;
-	if (is_admin() && (is_ukfl_club() || is_ukfl_team())){
+	if (is_admin() && (is_ukfl_team() || is_ukfl_sub_team())){
 		return $wpdb->prefix."posts.post_title ASC";
 	}
 	return $sql;
@@ -116,19 +116,19 @@ function club_team_orderby($sql){
 
 
 //Set up admin tables for teams/clubs
-add_filter( 'manage_posts_columns', 'ukfl_club_custom_columns' );
-add_action( 'manage_posts_custom_column' , 'ukfl_club_show_columns', 10, 2 );
+add_filter( 'manage_posts_columns', 'ukfl_team_custom_columns' );
+add_action( 'manage_posts_custom_column' , 'ukfl_team_show_columns', 10, 2 );
 
-function ukfl_club_custom_columns($defaults) {
+function ukfl_team_custom_columns($defaults) {
 	global $wp_query;
-	if (is_ukfl_club()){
+	if (is_ukfl_team()){
 		$defaults['club_logo'] = 'Club Logo';
 		$defaults['author'] = 'Team Captain';
 		return $defaults;
 	}
 	return $defaults;
 }
-function ukfl_club_show_columns($column, $post_id){
+function ukfl_team_show_columns($column, $post_id){
 	switch ($column) {
 		case 'club_logo':
 			echo get_the_post_thumbnail($post_id, 'thumbnail');
