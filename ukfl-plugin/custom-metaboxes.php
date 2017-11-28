74ea5dcf4c2ae3b02ebe5b13c4f0492fd99b2566
Ellen Schofield <tottlefields@gmail.com>
2017-11-28 22:27:02 +0000

    relpaceing club with team

--- ukfl-plugin/custom-metaboxes.php
+++ ukfl-plugin/custom-metaboxes.php
@@ -1,6 +1,6 @@
 <?php
 
-function custom_club_meta_box_markup($object){
+function custom_team_meta_box_markup($object){
 wp_nonce_field(basename(__FILE__), "meta-box-nonce");
 
     ?>
@@ -57,57 +57,57 @@ wp_nonce_field(basename(__FILE__), "meta-box-nonce");
     
 }
 
-function add_club_custom_meta_box(){
-    add_meta_box("club-meta-box", "Team Details", "custom_club_meta_box_markup", "ukfl_club", "side", "default", null);
+function add_team_custom_meta_box(){
+    add_meta_box("team-meta-box", "Team Details", "custom_team_meta_box_markup", "ukfl_team", "side", "default", null);
 }
 
 
-function custom_team_meta_box_markup($post) {
+function custom_sub_team_meta_box_markup($post) {
 	wp_nonce_field(basename(__FILE__), "meta-box-nonce");
 	
-	$clubs = get_posts(
+	$teams = get_posts(
 		array(
-			'post_type'   => 'ukfl_club',
+			'post_type'   => 'ukfl_team',
 			'orderby'     => 'title',
         	'order'       => 'ASC',
         	'numberposts' => -1
 		)
 	);
 	
-	if ( !empty( $clubs ) ) {
+	if ( !empty( $teams ) ) {
 		echo '<select name="parent_id" class="widefat">'; // !Important! Don't change the 'parent_id' name attribute.
 		echo '<option value="0">None</option>';
-		foreach ( $clubs as $club ) {
-			printf( '<option value="%s"%s>%s</option>', esc_attr( $club->ID ), selected( $club->ID, $post->post_parent, false ), esc_html( $club->post_title ) );
+		foreach ( $teams as $team ) {
+			printf( '<option value="%s"%s>%s</option>', esc_attr( $team->ID ), selected( $team->ID, $post->post_parent, false ), esc_html( $team->post_title ) );
 		}
 		echo '</select>';
 	}
 }
 
-function add_team_custom_meta_box($post){
-    add_meta_box("team-meta-box", "Parent Club", "custom_team_meta_box_markup", "ukfl_team", "side", "core", null);
+function add_sub_team_custom_meta_box($post){
+    add_meta_box("sub-team-meta-box", "Parent team", "custom_sub_team_meta_box_markup", "ukfl_sub-team", "side", "core", null);
 }
 
 function custom_event_meta_box_markup($post){	
         wp_nonce_field(basename(__FILE__), "meta-box-nonce");
 
 	$postmeta = get_post_meta( $post->ID );
-	$clubs = get_posts(
+	$teams = get_posts(
                 array(
-                        'post_type'   => 'ukfl_club',
+                        'post_type'   => 'ukfl_team',
                         'orderby'     => 'title',
                 'order'       => 'ASC',
                 'numberposts' => -1
                 )
         );
 
-	$selected = isset( $postmeta['event_host_club'] ) ? esc_attr( $postmeta['event_host_club'][0] ) : '';
+	$selected = isset( $postmeta['event_host_team'] ) ? esc_attr( $postmeta['event_host_team'][0] ) : '';
 
-	echo '<label for="event_host_club">Host Club</label>';
-	echo '<select name="event_host_club">';
+	echo '<label for="event_host_team">Host team</label>';
+	echo '<select name="event_host_team">';
 	printf('<option value="%s"%s>%s</option>', esc_attr( 'ukfl' ), selected( $selected, 'ukfl', false ), esc_html( 'UK Flyball League' ) );
-	foreach ( $clubs as $club ) {
-        	printf( '<option value="%s"%s>%s</option>', esc_attr( $club->ID ), selected( $selected, $club->ID, false ), esc_html( $club->post_title ) );
+	foreach ( $teams as $team ) {
+        	printf( '<option value="%s"%s>%s</option>', esc_attr( $team->ID ), selected( $selected, $team->ID, false ), esc_html( $team->post_title ) );
         }
         echo '</select>';
 }
@@ -125,14 +125,14 @@ function save_event_custom_meta_box($post_id){
 	// Exits script depending on save status
 	if ( $is_autosave || $is_revision || !$is_valid_nonce ) { return; }
 
-	if( isset( $_POST['event_host_club'] ) )
-		update_post_meta( $post_id, 'event_host_club', esc_attr( $_POST['event_host_club'] ) );
+	if( isset( $_POST['event_host_team'] ) )
+		update_post_meta( $post_id, 'event_host_team', esc_attr( $_POST['event_host_team'] ) );
 }
 
 
 
-add_action("add_meta_boxes", "add_club_custom_meta_box");
 add_action("add_meta_boxes", "add_team_custom_meta_box");
+add_action("add_meta_boxes", "add_sub_team_custom_meta_box");
 add_action("add_meta_boxes", "add_event_custom_meta_box");
 
 add_action("save_post", "save_event_custom_meta_box");
