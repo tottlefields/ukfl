7988428c117f387540f7128184c717a114beedd9
Ellen Schofield <tottlefields@gmail.com>
2017-11-28 22:31:35 +0000

    correct clubs for teams

--- ukfl-theme/general.php
+++ ukfl-theme/general.php
@@ -1,29 +1,29 @@
 <?php
 
-function get_clubs_for_user(){
+function get_teams_for_user(){
 	global $wpdb, $current_user;
 	$args = array(
-		'author'        =>  $current_user->ID,
-		'post_type'	=> 'ukfl_club',
-		'orderby'       =>  'post_title',
-		'order'         =>  'ASC',
-		'posts_per_page' => -1 // no limit
+			'author'        =>  $current_user->ID,
+			'post_type'		=> 'ukfl_team',
+			'orderby'       =>  'post_title',
+			'order'         =>  'ASC',
+			'posts_per_page' => -1 // no limit
 	);
-	$clubs = get_posts( $args );
-	return $clubs;
+	$teams = get_posts( $args );
+	return $teams;
 }
 
-function get_events_for_clubs($club_ids){
+function get_events_for_teams($team_ids){
 	$args = array(
-		'post_type'     => 'ukfl_event',
-		'posts_per_page' => -1,
-		'order'     => 'ASC',
-		'meta_key' => 'event_start_date',
-            	'orderby'   => 'meta_value_num',
-		'meta_query' => array(
-			array('key' => 'event_host_club', 'value' => $club_ids, 'compare' => 'IN'),
-			array('key' => 'event_start_date', 'value' => date('Ymd'), 'compare' => '>=')
-		)
+			'post_type'     => 'ukfl_event',
+			'posts_per_page' => -1,
+			'order'			=> 'ASC',
+			'meta_key'		=> 'event_start_date',
+			'orderby'   => 'meta_value_num',
+			'meta_query' => array(
+					array('key' => 'event_host_team', 'value' => $team_ids, 'compare' => 'IN'),
+					array('key' => 'event_start_date', 'value' => date('Ymd'), 'compare' => '>=')
+			)
 	);
 	$events = get_posts($args);
 	return $events;
