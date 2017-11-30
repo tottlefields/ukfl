a42f6d53b3af93d25928d551ee4628f480e13403
Ellen Schofield <tottlefields@gmail.com>
2017-11-30 13:49:56 +0000

    updates to dog page

--- ukfl-theme/page-templates/content-ukfl_dog.php
+++ ukfl-theme/page-templates/content-ukfl_dog.php
@@ -2,7 +2,6 @@
         <span class="site-author">
                 <figure class="avatar">
                 	<?php $author_id=$post->post_author; ?>
-                	<a data-tip="<?php the_author() ;?>" href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) );?>" data-toggle="tooltip" title="<?php echo the_author_meta( 'display_name' , $author_id ); ?>">
                 	<?php echo get_avatar( get_the_author_meta( 'ID' ) ); ?></a>
                 </figure>
         </span>
@@ -10,15 +9,18 @@
                 <header class="entry-header">
 			<h3  class="entry-title">Owner: <em><?php echo the_author_meta( 'display_name' , $author_id ); ?></em></h3>
                 </header>
+                
+                <?php $breeds = get_the_terms( get_the_ID(), 'dog-breeds' ); $breed = $breeds[0]; ?>
 
                 <div class="entry-meta">
-                        <span class="entry-date">Registered: <time datetime=""><?php the_time('M j,Y');?></time></span>
-                        <span class="entry-breed">Breed: <?php ?></span>
+                        <!-- <span class="entry-date">Registered: <time datetime=""><?php the_time('M j,Y');?></time></span> -->
+                        <span class="entry-ukfl">UKFL No: <?php get_the_title(); ?></span>
+                        <span class="entry-breed">Breed: <?php echo $breed->name; ?></span>
 				</div>
 		</div>
-               <div class="col-md-4 hidden-sm hidden-xs">
-                          <div style="float:right;"><a  href="<?php the_permalink(); ?>" class="post-thumbnail"><?php the_post_thumbnail(); ?></a></div>
-                </div>
+		<div class="col-md-4 hidden-sm hidden-xs">
+			<div style="float:right;"><a  href="<?php the_permalink(); ?>" class="post-thumbnail"><?php the_post_thumbnail(); ?></a></div>
+		</div>
         <div class="entry-content">
         </div>
 </article>
