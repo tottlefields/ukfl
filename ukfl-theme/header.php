770604ce963e7c8e216bbe2af7b288e2d6133d3f
Ellen Schofield <tottlefields@gmail.com>
2017-11-30 16:19:09 +0000

    added ukfl favicon

--- ukfl-theme/header.php
+++ ukfl-theme/header.php
@@ -9,7 +9,27 @@
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
 
-<?php wp_head(); ?>	
+<?php wp_head(); ?>
+
+<?php $favicon_dir = get_template_directory_uri() . '/assest/favicon'; ?>
+
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo $favicon_dir; ?>/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo $favicon_dir; ?>/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo $favicon_dir; ?>/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo $favicon_dir; ?>/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo $favicon_dir; ?>/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo $favicon_dir; ?>/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo $favicon_dir; ?>/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $favicon_dir; ?>/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo $favicon_dir; ?>/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192" href="<?php echo $favicon_dir; ?>/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo $favicon_dir; ?>/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="<?php echo $favicon_dir; ?>/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo $favicon_dir; ?>/favicon-16x16.png">
+	<link rel="manifest" href="<?php echo $favicon_dir; ?>/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="<?php echo $favicon_dir; ?>/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
 </head>
 <?php
 	$current_options = wp_parse_args(  get_option( 'busiprof_pro_theme_options', array() ), theme_data_setup() );
