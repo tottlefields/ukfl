194b5f8eac3b09499033ce47e95c94f211d38b8f
Ellen Schofield <tottlefields@gmail.com>
2017-11-30 12:39:19 +0000

    removed debugiing echo statments

--- ukfl-plugin/functions.php
+++ ukfl-plugin/functions.php
@@ -32,10 +32,8 @@ function generate_ukfl_number(){
 function generate_ukfl_dog_number($owner_ukfl){
 	global $wpdb;
 	$sql = "select group_concat(post_id) as post_ids from {$wpdb->prefix}postmeta where meta_key='ukfl_dog_owner' and meta_value='".$owner_ukfl."' group by meta_key,meta_value";
-	echo $sql."<br />";
 	$post_ids = $wpdb->get_var( $sql );
-	$sql = "select max(meta_value) from {$wpdb->prefix}postmeta where meta_key='ukfl_dog_letter' and post_id in ('.$post_ids.')";
-	echo $sql."<br />";
+	$sql = "select max(meta_value) from {$wpdb->prefix}postmeta where meta_key='ukfl_dog_letter' and post_id in ($post_ids)";
 	//$sql = "select max(dog_ukfl) from {$wpdb->prefix}ukfl_dogs where owner_ukfl='".$handler_ukfl."'";
 	$ukfl_no = $wpdb->get_var( $sql );
 	if (!isset($ukfl_no)){ $ukfl_no = 'A'; }
