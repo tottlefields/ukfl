4165a1b93256b818239f03af6eafacf566f76e09
Ellen Schofield <tottlefields@gmail.com>
2017-12-20 16:47:24 +0000

    changed DD for one-off payment; fixed host team and venue

--- ukfl-theme/page-templates/event_registration.php
+++ ukfl-theme/page-templates/event_registration.php
@@ -21,8 +21,8 @@ $admin_msg = 'New event registration on '.get_bloginfo('name').':<br /><br />
 	Team Captain: <strong>'.$current_user->user_firstname.' '.$current_user->user_lastname.'</strong><br />
 	Email Address: <strong>'.$current_user->user_email.'</strong><br />
 	UKFL Number: <strong>'.$current_user->user_login.'</strong><br /><br />
-	Host Team: <strong>'.$team_name.'</strong><br /><br />
-	Venue: <strong>'.$_POST['ukfl_event_venue'].', '.$_POST['ukfl_event_postcode'].'</strong><br /><br />';
+	Host Team: <strong>'.get_the_title(wp_get_post_parent_id($event->ID)).'</strong><br /><br />
+	Venue: <strong>'.get_post_meta($event->ID, 'ukfl_event_venue', 1).', '.get_post_meta($event->ID, 'ukfl_event_postcode', 1).'</strong><br /><br />';
 $headers = array('Content-Type: text/html; charset=UTF-8', 'Cc:'.get_option('admin_email'));
 //wp_mail('secretary@ukflyball.org.uk', '['.get_bloginfo('name').'] New Event Registration', $admin_msg, $headers);
 wp_mail('online@ukflyball.org.uk', '['.get_bloginfo('name').'] New Event Registration', $admin_msg, $headers);
@@ -40,9 +40,9 @@ get_template_part('index', 'bannerstrip');
 				<div class="page-content">
 						<article id="page-<?php the_ID(); ?>" <?php post_class('page'); ?> > 			
 							<div class="entry-content">
-								<p>Many thanks for registering <strong><?php echo get_post_meta( $event->ID, 'ukfl_dog_name', true ); ?></strong> as a new UKFL&copy; Event. Your request has been forwarded to the UKFL&copy; Secretary for approval. We will be in contact with you as soon as possible.</p>
-								<p>Your direct debit mandate and payment plan have been successfully setup (see reference below).</p>
-								<div class="alert alert-success" role="alert"><?php the_post(); the_content(); ?></div>
+								<p>Many thanks for registering <strong><?php echo get_post_meta( $event->ID, 'ukfl_event_title', true ); ?></strong> as a new UKFL&copy; Event. Your request has been forwarded to the UKFL&copy; Secretary for approval. We will be in contact with you as soon as possible.</p>
+								<p>Your one-off payment has been successfully setup.</p>
+								<div class="alert alert-success" role="alert"><?php the_post(); the_content(); ?></div> 
 								<p>Please <a href="/account/">click here</a> to return to your account.</p>
 							</div>
 					</article>
