3ba780a5263bf4c9789b0560db11298451b72d6e
Ellen Schofield <tottlefields@gmail.com>
2018-01-29 19:19:02 +0000

    stats

--- ukfl-theme/page-templates/stats-seedings.php
+++ ukfl-theme/page-templates/stats-seedings.php
@@ -38,7 +38,8 @@ if ( $seedings ) { ?>
 							$position = 1;
 							foreach ( $seedings as $team ){
 								$club = get_post($team->club_id);
-								echo '<tr><td>'.ordinal($position).'</td>';
+								echo '<tr><td>'.$position.'</td>';
+								echo '<td>'.ordinal($position).'</td>';
 								echo '<td><a href="'.get_permalink($team->club_id).'">'.$team->team_name.'</a></td>';
 								echo '<td>'.$team->fastest_time.'</td>';
 								echo '<td>'.$team->event_title.'</td></tr>';							
@@ -68,7 +69,12 @@ else{
 if ( $seedings ) { ?>
 <script type="text/javascript">
 jQuery(function ($) {
-	$('#seeding_list').DataTable();
+	$('#seeding_list').DataTable(
+		"ordering": false,
+		"columnDefs": [
+		    { "visible": false, "targets": 0 }
+		  ]
+	);
 });
 </script>
 <?php } ?>
