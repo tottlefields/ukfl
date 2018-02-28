cbd95d9c5f4a332dc1c373dc0fa9f91a760c2e48
Ellen Schofield <tottlefields@gmail.com>
2018-02-28 19:07:03 +0000

    ajax points uplaoder written

--- ukfl-theme/ajax.php
+++ ukfl-theme/ajax.php
@@ -12,17 +12,66 @@ function upload_points(){
 
 	ini_set("auto_detect_line_endings", true);
 
-	$event_date = dateToSQL($_POST['event_date']);
-	$event_id = $_POST['event'];
+	$DATE = dateToSQL($_POST['event_date']);
+	$EVENT = $_POST['event'];
 
 	$fh = fopen($_FILES['portal_file']['tmp_name'], 'rb');
 	$first_line = fgets($fh);
 	if (count(explode("\t", $first_line)) > 1){ echo json_encode(array('error' => "File is tab-delimited. Please re-save as comma-separated (CSV) and try uploading again.")); wp_die(); }
 	else{
-		while (($line = fgetcsv($fh, 1000, ',')) !== false){
-		        echo json_encode(array('event_date' => $event_date, 'event_id' => $event_id, 'first_line' => $line));
-	        	wp_die();
-		}
+		$wpdb->delete('ukfl_dog_points', array('event_id' => $EVENT, 'event_date' => $DATE));
+		$errors = array();
+		$warnings = array();
+		$success_count = 0;
+    	while (($data = fgetcsv($fh, 1000, ",")) !== FALSE) {
+	        //echo json_encode(array('event_date' => $event_date, 'event_id' => $event_id, 'first_line' => $line));
+	        if($data[3] == 'points' || $data[0] == ''){ continue; }
+	        
+			$dogs = get_posts(array('name' => $data[0], 'post_type' => 'ukfl_dog', 'post_status' => 'publish', 'post_status' => 'publish'));
+			if (!$dogs){ array_push($errors, "ERROR - no dog found for UKFL number $data[0]"); continue; }
+			$dog = $dogs[0];
+
+			$teams = get_posts(array('name' => $data[1], 'post_type' => 'ukfl_sub-team', 'post_status' => 'publish', 'post_status' => 'publish'));
+			if (!$teams){ array_push($errors, "ERROR - no team found with name of $data[1]"); continue; }
+			$team = $teams[0];
+
+			if ($data[2] == 'League'){
+
+				if ($team->post_parent != $dog->post_parent || $dog->post_parent == 0){
+					$team_date = DateTime::createFromFormat('Ymd', get_post_meta( $dog->ID, 'ukfl_team_joined', true ));
+					if($team_date){ 	//check 90 day period for a dog held to a team
+						array_push($warnings, "TODO - need to check old ukfl_team_joined date for dog number $data[0]");
+						continue;
+					}
+
+					array_push($warnings, "NOTE - dog $data[0] assigned to new team - ".get_the_title( $team->post_parent ));
+					update_post_meta($dog->ID, 'ukfl_team_joined', $DATE);
+					$wpdb->update( $wpdb->posts, array('post_parent' => $team->post_parent), array('ID' => $dog->ID) );
+				}
+				else{
+					$team_date = DateTime::createFromFormat('Ymd', get_post_meta( $dog->ID, 'ukfl_team_joined', true ));
+	                if(!$team_date){
+						// dog hasn't raced yet at all, set first date racing with team
+						update_post_meta($dog->ID, 'ukfl_team_joined', $DATE);
+	                }					
+				}				
+			}
+//	    	else{ echo "Team Type = ".$data[2]."\n"; }
+
+			//If get here then safe to load points!
+			$wpdb->insert( 'ukfl_dog_points', array('event_id' => $EVENT, 'event_date' => $DATE, 'team' => $data[1], 'team_type' => $data[2], 'dog_ukfl' => strtolower($data[0]), 'points' => $data[3]) );
+			$success_count++;
+    	}
+
+    	fclose($fh);
+    	
+ 		$return = array();
+ 		if(count($errors) > 0){ $return['errors'] = $errors; }
+ 		if(count($warnings) > 0){ $return['warnings'] = $warnings; }
+ 		$return['success'] = $success_count;
+    	
+ 		echo json_encode($return);
+	    wp_die();
 	}
 }
 
